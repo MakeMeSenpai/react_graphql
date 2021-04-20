bddfc67c705bafa0bbfb0e688ecacb23ce086fae
Anthony <sean.a.protho1@gmail.com>
2021-04-20 11:02:55 -0700

    stuck on 9

--- src/Weather/weather.js
+++ src/Weather/weather.js
@@ -1,17 +1,50 @@
 import { useState } from 'react'
 import { gql } from '@apollo/client'
 import { client } from '../index'
+import './weather.css'
 
 function Weather() {
     const [ zip, setZip ] = useState('')
+    // const [ units, setUnits] = useState('')
     const [ weather, setWeather ] = useState(null)
-  
     return (
         <div className="Weather">
 
-            {weather ? <h1>
+            {weather ? <div className="contain"><h1>
               {weather.data.getWeather.temperature}
-            </h1>: null}
+            </h1>
+
+            <h1>
+              {weather.data.getWeather.description}
+            </h1>
+
+            <details>
+              <summary>More </summary>
+              <h2>
+                {weather.data.getWeather.feels_like}
+              </h2>
+
+              <h2>
+                {weather.data.getWeather.temp_min}
+              </h2>
+
+              <h2>
+                {weather.data.getWeather.temp_max}
+              </h2>
+
+              <h2>
+                {weather.data.getWeather.humidity}
+              </h2>
+
+              <h2>
+                {weather.data.getWeather.cod}
+              </h2>
+
+              <h2>
+                {weather.data.getWeather.message}
+              </h2>
+            </details></div>
+            : null}
 
             <form onSubmit={(e) => {
                 e.preventDefault()
@@ -20,6 +53,10 @@ function Weather() {
                     value={zip}
                     onChange={(e) => setZip(e.target.value)}
                 />
+                {/* <label>Metric</label>
+                <input type="radio" name="units" value="metric" onClick={() => setUnits.value = 'metric'}/>
+                <label>Imperial</label>
+                <input type="radio" name="units" value="imperial" onClick={() => setUnits.value = 'imperial'}/> */}
                 <button type="submit">Submit</button>
             </form>
         </div>
@@ -30,9 +67,15 @@ function Weather() {
           const json = await client.query({
             query: gql`
               query {
-                getWeather(zip:${zip}) {
+                getWeather(zip:${zip}, units:imperial) {
                   temperature
                   description
+                  feels_like
+                  temp_min
+                  temp_max
+                  humidity
+                  cod
+                  message
                 }
               }
             `
